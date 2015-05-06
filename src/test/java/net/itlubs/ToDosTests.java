3020ff813d12b9e0b0c17949a475d95f3a897c8c
mshykov <maksimshykov@gmail.com>
2015-05-06 17:09:57 +0300

    work PC
    Code formating

--- src/test/java/net/itlubs/ToDosTests.java
+++ src/test/java/net/itlubs/ToDosTests.java
@@ -14,42 +14,42 @@ import static com.codeborne.selenide.Selenide.open;
  * Verifications have not developed yet
  */
 public class ToDosTests {
-
-	private void addTask(String str) {
+        
+    private void addTask(String str) {
         todoBlock.setValue(str).pressEnter();
-	}
-	
-	private void clearCompletedTasks() {
+    }
+        
+    private void clearCompletedTasks() {
         $("#clear-completed").click();
-	}
-	
-	@Test
+    }
+        
+    @Test
     public void testCreateTask() {
         String textToVerify1 = "abcdABCD1234!@#$";
         String textToVerify2 = "_+)({}|\"¥¨›„¨ŽººÒ><>?:|}{";
         String textToVerify3 = "text text text text TEXT TEXT TEXT";
         String textToVerify4 = "\'kashdkjahdkjqhekjahkjahdkjhzkjchkjahsdkjwhekja\'";
-		
+        
         ElementsCollection todoBlock = $$("#todo-list>li");
-		
+        
         //Step 1: Open todomvc.com in the web broweser
         open("http://todomvc.com/examples/troopjs_require/#/");
         //[VERIFICATION]: list is empty
         todoBlock.shouldBe(empty);
-
+        
         //Step 2: Add all items to the list
         addTask(textToVerify1);
         addTask(textToVerify2);
         addTask(textToVerify3);
         addTask(textToVerify4);
-
+        
         //Step 3: Remove 2nd task from the list
         todoBlock.get(1).hover().find(".destroy").click();
-
+        
         //Step 4: Check the last one as completed and clear all completed tasks
         todoBlock.get(2).find("#toggle").click();
         clearCompletedTasks();
-
+        
         //Step 5: Mark all task as completed and clear all completed tasks
         $("#toggle-all").click();
         clearCompletedTasks();
