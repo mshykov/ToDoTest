60ec2145ddb22ec5ec4a5312f4cfd059270fb7a0
mshykov <maksimshykov@gmail.com>
2015-05-06 14:19:45 +0300

    Commit from work PC
    Version 0.2
    Added a few methods, updated String values

--- src/test/java/net/itlubs/ToDosTests.java
+++ src/test/java/net/itlubs/ToDosTests.java
@@ -10,41 +10,46 @@ import static com.codeborne.selenide.Selenide.open;
 
 /**
  * Created by Shykov Maksym on 29.04.15.
- * Version 0.1
+ * Version 0.2
  * Verifications have not developed yet
  */
 public class ToDosTests {
 
     @Test
     public void testCreateTask() {
-        String textToVerify1 = "text 1";
-        String textToVerify2 = "text 2";
-        String textToVerify3 = "text 3";
-        String textToVerify4 = "text 4";
-
+		String textToVerify2 = "_+)({}|\"¥¨›„¨ŽººÒ><>?:|}{";
+		
+		ElementsCollection todoBlock = $$("#todo-list>li");
+		
+		private void addTask(String str) {
+			todoBlock.setValue(str).pressEnter();
+		}
+		
+		private void clearCompletedTasks() {
+			$("#clear-completed").click();
+		}
+		
         //Step 1: Open todomvc.com in the web broweser
         open("http://todomvc.com/examples/troopjs_require/#/");
-        //[VERIFICATION]: list is empty
-        $$("#todo-list>li").shouldBe(empty);
+		//[VERIFICATION]: list is empty
+        todoBlock.shouldBe(empty);
 
         //Step 2: Add all items to the list
-        $("#new-todo").setValue(textToVerify1).pressEnter();
-        $("#new-todo").setValue(textToVerify2).pressEnter();
-        $("#new-todo").setValue(textToVerify3).pressEnter();
-        $("#new-todo").setValue(textToVerify4).pressEnter();
+		addTask(textToVerify1);
+		addTask(textToVerify2);
+		addTask(textToVerify3);
+		addTask(textToVerify4);
 
         //Step 3: Remove 2nd task from the list
-        SelenideElement el = $$("#todo-list>li").get(1);
-        el.hover();
-        el.find(".destroy").click();
+        todoBlock.get(1).hover().find(".destroy").click();
 
         //Step 4: Check the last one as completed and clear all completed tasks
-        $$("#todo-list>li").get(2).find("[type=checkbox]").click();
-        $("#clear-completed").click();
+        todoBlock.get(2).find("#toggle").click();
+        clearCompletedTasks();
 
         //Step 5: Mark all task as completed and clear all completed tasks
         $("#toggle-all").click();
-        $("#clear-completed").click();
+        clearCompletedTasks();
 
-    }
+	}
 }
