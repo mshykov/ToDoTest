baf8b252c35d8f3a2b88f44dd2e82409266af434
mshykov <maksimshykov@gmail.com>
2015-05-07 01:04:07 +0300

    refactored

--- src/test/java/net/itlubs/ToDosTests.java
+++ src/test/java/net/itlubs/ToDosTests.java
@@ -34,26 +34,24 @@ public class ToDosTests {
         }
     }
 
-    public void isTextAddedToList(int size, String str) {
+    public void assertTextAddedToList(int size, String str) {
         int getElement = size - 1;
 
         todoBlock.shouldHaveSize(size);
-        if (str != null) {
-            todoBlock.get(getElement).shouldHave(text(str));
-        }
+        todoBlock.get(getElement).shouldHave(text(str));
     }
 
-    public void isFooterExists(String str) {
+    public void assertFooterExists(String str) {
         footer.shouldBe(present);
         countElements.shouldHave(text(str));
     }
         
     @Test
     public void testCreateTask() {
-        String textToVerify1 = "abcdABCD1234!@#$";
-        String textToVerify2 = "_+)({}|\"¥¨›„¨ŽººÒ><>?:|}{";
-        String textToVerify3 = "text text text text TEXT TEXT TEXT";
-        String textToVerify4 = "\'kashdkjahdkjqhekjahkjahdkjhzkjchkjahsdkjwhekja\'";
+        String t1 = "abcdABCD1234!@#$";
+        String t2 = "_+)({}|\"¥¨›„¨ŽººÒ><>?:|}{";
+        String t3 = "text text text text TEXT TEXT TEXT";
+        String t4 = "\'kashdkjahdkjqhekjahkjahdkjhzkjchkjahsdkjwhekja\'";
 
         //Step 1: Open todomvc.com in the web broweser
         open("http://todomvc.com/examples/troopjs_require/#/");
@@ -62,29 +60,28 @@ public class ToDosTests {
         footer.shouldBe(hidden);
 
         //Step 2: Add all items to the list
-        addTask(textToVerify1);
-        isTextAddedToList(1, textToVerify1);
-        isFooterExists("1");
-        addTask(textToVerify2);
-        addTask(textToVerify3);
-        addTask(textToVerify4);
-        isTextAddedToList(4, textToVerify4);
-        isFooterExists("4");
+        addTask(t1);
+        assertTextAddedToList(1, t1);
+        assertFooterExists("1");
+        addTask(t2);
+        addTask(t3);
+        addTask(t4);
+        assertTextAddedToList(4, t4);
+        assertFooterExists("4");
 
         //Step 3: Remove 2nd task from the list
-        SelenideElement lineToDelete = todoBlock.get(1);
-        lineToDelete.hover();
-        lineToDelete.find(".destroy").click();
+        todoBlock.get(1).hover();
+        todoBlock.get(1).find(".destroy").click();
         //[VERIFICATION]: 2nd task deleted; footer updated
-        isTextAddedToList(3, null);
-        isFooterExists("3");
+        todoBlock.shouldHaveSize(3);
+        assertFooterExists("3");
         
         //Step 4: Check the last one as completed and clear all completed tasks
         todoBlock.get(2).find(".toggle").click();
         clearCompletedTasks();
         //[VERIFICATION]: 4th task removed from the list; footer updated
-        isTextAddedToList(2, null);
-        isFooterExists("2");
+        todoBlock.shouldHaveSize(2);
+        assertFooterExists("2");
 
         //Step 5: Mark all task as completed and clear all completed tasks
         $("#toggle-all").click();
